7e77c85688163b458898c2f215b76b5905a48349
Junrong Yu <junrong.yu@uqconnect.edu.au>
2022-01-07 12:11:45 +1000

    First commit (upload)
    This is the first commit for this project!

---
+++ script.js
@@ -0,0 +1,169 @@
+function firstUP(){
+    var prevNum = parseInt(document.getElementById("first").textContent);
+    document.getElementById("first").innerHTML = prevNum+1;
+}
+
+function firstDOWN(){
+    var prevNum = parseInt(document.getElementById("first").textContent);
+    document.getElementById("first").innerHTML = prevNum-1;
+}
+
+function secondUP(){
+    var prevNum = parseInt(document.getElementById("second").textContent);
+    document.getElementById("second").innerHTML = prevNum+1;
+}
+
+function secondDOWN(){
+    var prevNum = parseInt(document.getElementById("second").textContent);
+    document.getElementById("second").innerHTML = prevNum-1;
+}
+
+// dice related
+
+var cube = document.getElementById('cube');
+
+var min = 1;
+var max = 24;
+
+cube.onclick = function() {
+  var xRand = getRandom(max, min);
+  var yRand = getRandom(max, min);
+    
+  cube.style.webkitTransform = 'rotateX('+xRand+'deg) rotateY('+yRand+'deg)';
+  cube.style.transform = 'rotateX('+xRand+'deg) rotateY('+yRand+'deg)';
+}
+
+function getRandom(max, min) {
+  return (Math.floor(Math.random() * (max-min)) + min) * 90;
+}
+
+// timer-related
+
+const FULL_DASH_ARRAY = 283;
+const WARNING_THRESHOLD = 10;
+const ALERT_THRESHOLD = 5;
+
+const COLOR_CODES = {
+  info: {
+    color: "green"
+  },
+  warning: {
+    color: "orange",
+    threshold: WARNING_THRESHOLD
+  },
+  alert: {
+    color: "red",
+    threshold: ALERT_THRESHOLD
+  }
+};
+
+var TIME_LIMIT = 15;
+
+function seconds(sel){
+    // alert(sel.options[sel.selectedIndex].text);
+    // return 30;
+    // var sec = parseInt(document.getElementById("input").textContent);
+    // return sec;
+    var sec = sel.options[sel.selectedIndex].text;
+    // return parseInt(sec);
+    TIME_LIMIT = parseInt(sec);
+}
+
+// const TIME_LIMIT = seconds();
+let timePassed = 0;
+let timeLeft = TIME_LIMIT;
+let timerInterval = null;
+let remainingPathColor = COLOR_CODES.info.color;
+
+document.getElementById("app").innerHTML = `
+<div class="base-timer">
+  <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <g class="base-timer__circle">
+      <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+      <path
+        id="base-timer-path-remaining"
+        stroke-dasharray="283"
+        class="base-timer__path-remaining ${remainingPathColor}"
+        d="
+          M 50, 50
+          m -45, 0
+          a 45,45 0 1,0 90,0
+          a 45,45 0 1,0 -90,0
+        "
+      ></path>
+    </g>
+  </svg>
+  <span id="base-timer-label" class="base-timer__label">${formatTime(
+    timeLeft
+  )}</span>
+</div>
+`;
+
+// startTimer();
+
+function onTimesUp() {
+  var audio = new Audio('https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3');
+  audio.play();
+  clearInterval(timerInterval);
+}
+
+function startTimer() {
+    timePassed = 0;
+  timerInterval = setInterval(() => {
+    timePassed = timePassed += 1;
+    timeLeft = TIME_LIMIT - timePassed;
+    document.getElementById("base-timer-label").innerHTML = formatTime(
+      timeLeft
+    );
+    setCircleDasharray();
+    setRemainingPathColor(timeLeft);
+
+    if (timeLeft === 0) {
+      onTimesUp();
+    }
+  }, 1000);
+}
+
+function formatTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
+  return `${minutes}:${seconds}`;
+}
+
+function setRemainingPathColor(timeLeft) {
+  const { alert, warning, info } = COLOR_CODES;
+  if (timeLeft <= alert.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(warning.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(alert.color);
+  } else if (timeLeft <= warning.threshold) {
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(info.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(warning.color);
+  }
+}
+
+function calculateTimeFraction() {
+  const rawTimeFraction = timeLeft / TIME_LIMIT;
+  return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+}
+
+function setCircleDasharray() {
+  const circleDasharray = `${(
+    calculateTimeFraction() * FULL_DASH_ARRAY
+  ).toFixed(0)} 283`;
+  document
+    .getElementById("base-timer-path-remaining")
+    .setAttribute("stroke-dasharray", circleDasharray);
+}
